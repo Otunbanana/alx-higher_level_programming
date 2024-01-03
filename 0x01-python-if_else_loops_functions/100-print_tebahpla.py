13157456048260ce417bb2205721e0c240f4cdea
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-03 19:41:42 +0100

    14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,4 +2,4 @@
 for i in range(122, 96, -1):
     print("{}".format(chr(i)), end="")
     if i - 1 >= 65:
-        print("{}".format(chr(i - 1)), end="")
+        print("{}".format(chr(i - 1).upper()), end="")
