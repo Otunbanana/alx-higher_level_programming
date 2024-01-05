c0ea0a8a1db6a2562a188fd526b96c8213453461
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-05 17:10:36 +0100

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
 
-num_args = len(argv) - 1
+if __name__ == "__main__":
 
-if num_args == 0:
-    print("0 arguments.")
-    print(".")
-else:
-    print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
+    from sys import argv
 
-    for i, arg in enumerate(argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+        print(".")
+    else:
+        print("{} argument{}:".format(num_args, 's' if num_args > 1 else ''))
+
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{}: {}".format(i, arg))
