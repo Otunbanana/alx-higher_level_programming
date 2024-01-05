37ffa75040f8adcad190611975570f9d0bec4887
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-05 19:26:04 +0100

    100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,6 +5,7 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(argv[1])
@@ -20,6 +21,7 @@ if __name__ == "__main__":
     elif operator == '/':
         result = div(a, b)
     else:
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     print("{} {} {} = {}".format(a, operator, b, result))
