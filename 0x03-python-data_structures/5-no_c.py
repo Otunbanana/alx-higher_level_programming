19022084736f649240536ec9e5165d708ce2e0b2
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-07 23:03:36 +0100

    5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,4 +2,4 @@
 def no_c(my_string):
     filtered_chars = (char for char in my_string if char.lower() not in ('c', 'C'))
     new_string = ''.join(filtered_chars)
-    return new_string
+    return (new_string)
