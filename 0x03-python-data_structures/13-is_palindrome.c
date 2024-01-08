800f0bbd221addce9cb9676e49389cda7abb7625
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-08 18:20:36 +0100

    13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,28 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-#include <stddef.h>
-
-/* Function prototype for reverse_list */
-listint_t *reverse_list(listint_t **head);
-int compare_lists(listint_t *list1, listint_t *list2);
 
 /**
 * is_palindrome - checks if a singly linked list is a palindrome
-* @head: pointer to the head of the linked list
-* Return: 1 if the linked list is a palindrome, 0 otherwise
+* @head: pointer to the head of the list
+* Return: 1 if palindrome, 0 otherwise
 */
 int is_palindrome(listint_t **head)
 {
-listint_t *slow = *head, *fast = *head, *prev_slow = *head;
-listint_t *second_half, *mid_node = NULL;
+listint_t *slow, *fast, *prev_slow, *mid_node;
 int is_palindrome = 1;
 
 if (*head == NULL || (*head)->next == NULL)
 return (1);
 
+slow = fast = *head;
+
 while (fast != NULL && fast->next != NULL)
 {
 fast = fast->next->next;
+
 prev_slow = slow;
+
 slow = slow->next;
 }
 
@@ -32,61 +32,44 @@ mid_node = slow;
 slow = slow->next;
 }
 
-second_half = reverse_list(&slow);
+prev_slow->next = NULL;
+prev_slow = NULL;
+while (slow != NULL)
+{
+fast = slow->next;
+slow->next = prev_slow;
+prev_slow = slow;
+slow = fast;
+}
 
-is_palindrome = compare_lists(*head, second_half);
+while (*head != NULL && prev_slow != NULL)
+{
+if ((*head)->n != prev_slow->n)
+{
+is_palindrome = 0;
+break;
+}
 
-reverse_list(&second_half);
+*head = (*head)->next;
+prev_slow = prev_slow->next;
+}
+
+slow = NULL;
+while (prev_slow != NULL)
+{
+fast = prev_slow->next;
+prev_slow->next = slow;
+slow = prev_slow;
+prev_slow = fast;
+}
 
 if (mid_node != NULL)
 {
+prev_slow = slow;
+slow = slow->next;
 prev_slow->next = mid_node;
-mid_node->next = second_half;
-}
-else
-{
-prev_slow->next = second_half;
+mid_node->next = slow;
 }
 
 return (is_palindrome);
-}
-
-/**
-* reverse_list - reverses a linked list
-* @head: pointer to the head of the linked list
-* Return: pointer to the new head of the reversed list
-*/
-listint_t *reverse_list(listint_t **head)
-{
-listint_t *prev = NULL, *current = *head, *next = NULL;
-
-while (current != NULL)
-{
-next = current->next;
-current->next = prev;
-prev = current;
-current = next;
-}
-
-*head = prev;
-return (*head);
-}
-
-/**
-* compare_lists - compares two linked lists for equality
-* @list1: first linked list
-* @list2: second linked list
-* Return: 1 if the lists are equal, 0 otherwise
-*/
-int compare_lists(listint_t *list1, listint_t *list2)
-{
-while (list1 != NULL && list2 != NULL)
-{
-if (list1->n != list2->n)
-return (0);
-list1 = list1->next;
-list2 = list2->next;
-}
-
-return (list1 == NULL && list2 == NULL);
 }
