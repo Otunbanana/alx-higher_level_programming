2e7536a7ecfe166c14590ae359833a4a8562f37f
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-23 23:49:02 +0100

    i have tried

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This module defines a MagicClass class.
+"""
+
+import math
+
+class MagicClass:
+    """
+    Class representing a magic circle.
+
+    Attributes:
+        __radius (float):representing the radius of the circle.
+    """
+    def __init__(self, radius=0):
+        """
+        Initializes a new MagicClass instance.
+
+        Parameters:
+            radius (float, optional): Radius of the circle.
+
+        Raises:
+            TypeError: If radius is not a number.
+        """
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        """
+        Computes and returns the area of the circle.
+
+        Returns:
+            float: Area of the circle.
+        """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """
+        Computes and returns the circumference of the circle.
+
+        Returns:
+            float: Circumference of the circle.
+        """
+        return 2 * math.pi * self.__radius
