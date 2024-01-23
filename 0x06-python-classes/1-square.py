d662cc1d37ba86f35798f9da82f0aa9d4fe27915
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-23 23:10:38 +0100

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    Class representing a square.
+
+    Attributes:
+        __size (int): Private instance attribute representing the size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+            size (int): Size of the square.
+
+        Note:
+            The size attribute is private and prefixed with "__".
+        """
+        self.__size = size
