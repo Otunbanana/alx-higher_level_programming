f9a7354925689f29a8c1802e6b575ae4039d746e
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-23 23:00:44 +0100

    00

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 0-square.py
-# Musa Otun <otunmusa@gmail.com>
 """This module defines a Square class."""
 
 class Square:
-    """Empty class representing a square."""
+"""Empty class representing a square."""
     pass
