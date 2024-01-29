faaf983f05403f3a8c318bcce4885a3a844a5bd7
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-29 19:46:07 +0100

    3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -115,4 +115,4 @@ class Rectangle:
         Returns:
             str: The string representation of the rectangle.
         """
-        return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({})".format(self.__width, self.__height)
