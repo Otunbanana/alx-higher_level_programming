da634c45507917af3fd1f8e1002e844172dbafa8
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-30 20:38:29 +0100

    7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -110,7 +110,6 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-
         symbol_rows = [str(self.print_symbol) * self.__width for _ in range(self.__height)]
         return "\n".join(symbol_rows)
 
