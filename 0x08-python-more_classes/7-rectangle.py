729b5af4160496ab5ca3ed94381cd8620ece603e
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-30 20:37:22 +0100

    07

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ Module for the Rectangle class.
 class Rectangle:
     """
     Defines a rectangle with private attributes width and height.
-    
+
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
         print_symbol (any): The symbol used for string representation.
