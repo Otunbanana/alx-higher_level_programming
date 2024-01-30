9c7689e087084df62bb86408045456e66ef1ce84
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-30 20:10:52 +0100

    7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,7 +64,6 @@ class Rectangle:
 
     def __str__(self):
         """Return the printable representation of the Rectangle.
-
         Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
