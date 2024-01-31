b34d126ad15dd4eeed182031010c7fcb4d3982b0
Otunbanana <87811466+Otunbanana@users.noreply.github.com>
2024-01-31 21:28:14 +0100

    test d

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds two integers.
+
+"""
+
+def add_integer(a, b=98):
+    """
+    Add two integers.
+
+    Args:
+        a (int or float): The first integer or float.
+        b (int or float): The second integer or float. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
